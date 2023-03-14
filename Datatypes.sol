505b72940842a8ccb51be033647761901f592d79
abhishekbhonde <abhishekbhonde567@gmail.com>
2023-03-14 17:15:23 +0530

    first code

---
+++ Datatypes.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+
+
+pragma solidity ^0.8.0;
+
+contract Datatypes{
+
+    bool public booltemp = true;
+
+    uint public temp = 243;
+
+    int public temp1 = 143;
+
+    address public addr = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+
+    bytes32 public bytestemp;
+}
